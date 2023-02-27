e470cdf241888d914b6062f869ea4d2b9d3f8b22
Arfaz Hussain <arfazhussain@uvic.ca>
2023-02-27 01:53:28 -0800

    Routine Commit by @arfazhxss on Mon 27 Feb 2023 01:53:28 PST

--- init.sh
+++
@@ -1,7 +0,0 @@
-echo "# robotC" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/arfazhxss/robotC.git
-git push -u origin main
