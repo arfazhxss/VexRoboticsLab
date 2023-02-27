ba39adb43398260050b2fccf9fde3a929150ec8d
ARFAZ-WINDOWS <arfazhussain@uvic.ca>
2023-02-27 01:28:12 -0800

    Routine Commit Session Committed on: [Mon Feb 27 01:28:12 PST 2023] by Arfaz

--- robot.c
+++ robot.c
@@ -11,33 +11,33 @@
 //
 //
 
-int motorSpeed = 50;											// (UNIT) centimeter, cm
+int motorSpeed = 50;                              // (UNIT) centimeter, cm
 
 void moveForward() {
 	/*MAIN*/
 	motor[leftMotor] = motorSpeed;
-	motor[rightMotor] = motorSpeed+5;							// calibrating with both motor speed
-	wait1Msec(2000);											// may differ by weight and circumstance, subject to change
+	motor[rightMotor] = motorSpeed+5;               // calibrating with both motor speed
+	wait1Msec(2000);                                // may differ by weight and circumstance, subject to change
 
-	/*STOP*/													// duration: 1 second
+	/*STOP*/                                        // duration: 1 second
 	motor[leftMotor] = 0;
 	motor[rightMotor] = 0;
 	wait1Msec(1000);
 
-	/* motorSpeed=-15;											// legacy test start
-	motor[leftMotor] = motorSpeed;								
-	motor[rightMotor] = motorSpeed-10;						
-	wait1Msec(3000); */											// legacy test end
+	/* motorSpeed=-15;											        // legacy test start
+	motor[leftMotor] = motorSpeed;
+	motor[rightMotor] = motorSpeed-10;
+	wait1Msec(3000); */                             // legacy test end
 
 }
 
 void turnRight() {
 	/*MAIN*/
 	motor[leftMotor] = motorSpeed;
-	motor[rightMotor] = 0;										// calibrating with both motor speed, duration: 1.3ish? seconds?
-	wait1Msec(1300);											// duration will differ by weight, circumstances, subject to change
+	motor[rightMotor] = 0;                          // calibrating with both motor speed, duration: 1.3ish? seconds?
+	wait1Msec(1300);                                // duration will differ by weight, circumstances, subject to change
 
-	/*STOP*/													// duration: 1 second
+	/*STOP*/                                        // duration: 1 second
 	motor[leftMotor] = 0;
 	motor[rightMotor] = 0;
 	wait1Msec(1000);
@@ -45,14 +45,14 @@ void turnRight() {
 }
 
 task main () {
-	while (1==1) {												// continuity clause
-		if (SensorValue[buttonToMove] == 0) {					// should be 1 when button is pressed according to documentation
-			moveForward();										// function, run - wait - run - stop
+	while (1==1) {                                  // continuity clause
+		if (SensorValue[buttonToMove] == 0) {         // should be 1 when button is pressed according to documentation
+			moveForward();                              // function, run - wait - run - stop
 		}
-		else if (SensorValue(buttonToTurn) == 0) {				// should be ... documentation
-			turnRight();										// function, run - wait - run - stop
+		else if (SensorValue(buttonToTurn) == 0) {    // should be ... documentation
+			turnRight();                                // function, run - wait - run - stop
 		}
-		else {													// default mode
+		else {													              // default mode
 			motor[leftMotor] = 0;
 			motor[rightMotor] = 0;
 		}
